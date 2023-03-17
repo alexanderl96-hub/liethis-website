d4145f946059ea4ce0d49264613a8dedba93f33a
Alexander <alexanderl96@yahoo.com>
2023-03-17 14:38:37 -0400

    adding css amedia query for home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-import './../NavBar/NavBar.css'
+// import './../NavBar/NavBar.css'
 import liethiPresentation from '../../Images/IMG_6550.JPG'
 // import liethiMiddle from '../../Images/FB_IMG_1450260439547.jpg'
 import liethiCreative from '../../Images/LiethisCreative.jpeg'
@@ -17,39 +17,32 @@ const Home = () => {
                 <img src={liethiPresentation} alt='Liethis' className='imag' />
             </div>
             <h3><em>"Performing arts is not a luxury but a necessity for humanity"</em></h3>
-            {/* <div className="divTextwrap">
-                <h2 className="divTextwraph2">The power of creative Art </h2>
-                Movement has healing properties  in general, dance  
-                has been known to heal people mentally, spiritually,  in some cases  physically.  
-                According to the article published by Shannon Sexton, “Dancing can boost mood, 
-                self-esteem, and fitness levels; reduce stress and even help us recover from trauma.“ 
-                I would like to offer a space where movement can help  humanity in an artistic, holistic  
-                form.
-             </div> */}
         </div>
 
-        <div className="" style={{display: 'flex', marginTop: '100px', paddingLeft: '3%',paddingRight: '3%',  marginBottom: '100px'}}>
+        <div className="mainDivWrap2" >
             <div className="homeImgDiv">
-                <img src={liethiCreative} alt='Liethis' className='imag' />
+                <img src={liethiCreative} alt='Liethis' className='imag2' />
             </div>
-            <div className="divTextwrap" style={{margin: 'auto',  backgroundColor: '#f2f2f2', height: '530px', }}>
-                <h2 className="divTextwraph2" style={{marginTop: '70px'}}>The power of creative Art </h2>
-                <div style={{fontWeight: 'normal', lineHeight: '1.5', wordSpaing: '1px',textAlign: 'justify', }}>
+            <div className="divTextwrap" >
+                <h2 className="divTextwraph2" >The power of creative Art </h2>
+                <div className='divTextwraph3'>
                 COMUNIDADES Afrodesendientes together, we can rewrite our story, amend what freedom is, and find joy in our shades. OUR NARRATIVES ARE HERE!</div>
             
-                <div style={{fontWeight: 'normal', lineHeight: '1.6', padding: '8%'}}>
+                <div className='divTextwraph4'>
                 "I would like to offer a space where movement can help  humanity in an artistic & holistic  
                 form."</div>
-                <div style={{display:'flex', textAlign: 'center', justifyContent: 'center', width: '180px',  
-                 backgroundColor: '#fdc03b', color: 'white', marginTop: '40px'}}><p style={{fontSize: '16px'}}>
-                    <a href="https://www.youtube.com/@Cubanclassrooms5/channels" style={{textDecoration:'none', color:'white'}}>Subscribe!</a></p></div>
+                <div className='div-buttomChannel' >
+                    <p >
+                       <a href="https://www.youtube.com/@Cubanclassrooms5/channels" >Subscribe!</a>
+                    </p>
+                </div>
              </div>
         </div>
         <div>
-                <div style={{fontSize: '20px'}}>Join our community.</div> 
-                <div style={{marginTop: '25px', fontWeight: '400'}}>WE LOVE DANCE, MUSIC, CREATIVE ART,  COMMUNITY, AND EMPOWERING WOMEN.</div>
-            </div>
-        <div style={{display: 'flex', justifyContent:'space-around',  marginBottom: '100px'}}>
+            <div className='joinComunity' >Join our community.</div> 
+            <div className='joinComunity-quote' >WE LOVE DANCE, MUSIC, CREATIVE ART,  COMMUNITY, AND EMPOWERING WOMEN.</div>
+        </div>
+        <div className='componentContact' >
            <Contact />
         </div>
        
