35f5b0ed76ec08728abe3856ef03698d76e63c8c
Alexander <alexanderl96@yahoo.com>
2023-03-22 16:37:21 -0400

    adding pdf download

--- src/Components/Afrocusoul/Service/Service.js
+++ src/Components/Afrocusoul/Service/Service.js
@@ -1,20 +1,58 @@
-import React, {useState} from 'react'
+import React, {useState, PureComponent} from 'react'
 import './Service.css'
+import jsPDF from 'jspdf'
+import imageDog from '../../../Images/imagesDog.jpeg'
 
 const numbersSquare = [1,2,3,4,5,6,7,8]
 
 const AfrosoulNYC = () => {
   const [classNumber, setClassNumber] = useState(0)
   const [open , setOpen] = useState('flase')
-  const image = 'https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_150,q_auto:low,fl_lossy,dpr_2.0,c_fill,f_auto,h_130,g_auto/u5af76jhqhcyvtng1rku'
+  // const image = 'https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_150,q_auto:low,fl_lossy,dpr_2.0,c_fill,f_auto,h_130,g_auto/u5af76jhqhcyvtng1rku'
 
   function handelId (e){
     if(Number(e.target.id)){
       setClassNumber(Number(e.target.id))
     }
   }
+
+  function  jsPdfGenerate (){
+    
+    let doc = new jsPDF('p','pt');
+  
+    doc.setFontSize(20)
+    doc.setFont('arial', 'cursive')
+    doc.text(200,50, 'Elsie`s Coffee-Shop')
+
+    doc.setFontSize(15)
+    doc.text(40,120, 'Client: Alexander La Rosa Perez')
+    doc.text(40,140, 'Order: 12345')
+    doc.text(40,160, 'Address: 626 Riderside Drive #24a New York, NY 10031')
+  
+    doc.addImage(imageDog, 'jpeg', 180,220,220,150, 10,10)
+
+    doc.text(40,450, 'Price: $12.00')
+    doc.text(40,470, 'Fee:    $0.30')
+    doc.text(40,490, 'Tax:    0.30%')
+    doc.text(40,510, 'Total:  $13.50')
+
+
+    doc.text(40, 600, 'Thanks for visiting our services!')
+    doc.text(40, 620, 'We wish you a wonderful day.')
+
+    doc.setFontSize(16)
+    doc.text(240, 700, 'Elsie`s Coffee-Shop')
+
+
+    doc.save('generate.pdf')
+  }
+
   return (
     <div>
+      <div 
+          onClick={jsPdfGenerate}
+           style={{margin: '20px', width: '150px', height: '50px', border: 'none', display: 'flex', boxShadow: '1px 1px 4px 3px #000000',
+                    justifyContent: 'center', alignItems: 'center', backgroundColor: '#20639b', borderRadius: '50px', cursor: 'pointer' }}>Download PDF</div>
         {/* <h1 className='title'></h1>
         <div className='afrosoulNYtext'> 
             
